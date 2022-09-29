d94ed279aba2b7e2f782f5fe81d5ec9530b9b1eb
DannyWorku1234 <85625760+DannyWorku1234@users.noreply.github.com>
2022-09-29 12:14:11 +0300

    Create 0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,21 +1,21 @@
 #!/usr/bin/env bash
-# Set up server file system for deployment
-
-# install nginx
-sudo apt-get -y update
-sudo apt-get -y install nginx
-sudo service nginx start
-
-# configure file system
-sudo mkdir -p /data/web_static/shared/
-sudo mkdir -p /data/web_static/releases/test/
-echo "Holberton School" | sudo tee /data/web_static/releases/test/index.html > /dev/null
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-
-# set permissions
+# script to set up clean server
+sudo apt-get update
+sudo apt-get install -y nginx
+sudo mkdir /data/
+sudo mkdir /data/web_static/
+sudo mkdir /data/web_static/releases/
+sudo mkdir /data/web_static/shared/
+sudo mkdir /data/web_static/releases/test/
+echo "<!DOCTYPE html>
+<html>
+  <head>
+  </head>
+  <body>
+    Best School
+  </body>
+</html>" | sudo tee /data/web_static/releases/test/index.html
+sudo ln -fs /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
-
-# configure nginx
-sudo sed -i '44i \\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}' /etc/nginx/sites-available/default
-
-# restart web server udo service nginx restart
+sudo sed -i '39 i\ \tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t}\n' /etc/nginx/sites-enabled/default
+sudo service nginx restart
